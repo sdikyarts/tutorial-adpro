f14675f71b42e32839b37beb8d3cbbc37a737cbe
sdikyarts <wtrcrss.family@gmail.com>
2025-02-11 17:28:32 +0700

    Adjusting Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "inkidatabaseid.ac.ui.cs.advprog"
+group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
