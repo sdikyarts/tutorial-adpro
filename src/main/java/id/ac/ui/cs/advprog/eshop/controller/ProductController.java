dd37d2a8492b34fea9b4b56e6108813aacad06f6
sdikyarts <wtrcrss.family@gmail.com>
2025-02-11 17:40:39 +0700

    Create Product controller

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/create")
+    public String createProductPage(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "createProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProductPost(@ModelAttribute("product") Product product, Model model) {
+        productService.create(product);
+        return "redirect:list";
+    }
+    
+    @GetMapping("/list")
+    public String listProducts(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "productList";
+    }
+}
