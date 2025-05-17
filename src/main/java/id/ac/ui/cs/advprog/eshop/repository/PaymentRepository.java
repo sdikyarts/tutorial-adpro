3738dc376fcb1218b2387cbc2968684552b7a8d1
sdikyarts <wtrcrss.family@gmail.com>
2025-05-17 09:54:57 +0700

    [RED] Create the Payment repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+public class PaymentRepository {
+    public void save(Payment payment) {
+    }
+
+    public Payment findById(String id) {
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        return null;
+    }
+}
