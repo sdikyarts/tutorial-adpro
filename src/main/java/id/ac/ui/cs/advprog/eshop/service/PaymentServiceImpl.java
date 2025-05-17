bc2df4634def02a8118225a0110ff7c7ce8efd01
sdikyarts <wtrcrss.family@gmail.com>
2025-05-17 10:02:31 +0700

    [RED] Create Payment Service skeletons

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class PaymentServiceImpl implements PaymentService {
+
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        return null;
+    }
+
+    public Payment setStatus(Payment payment, String status) {
+        return null;
+    }
+
+    public Payment getPayment(String paymentId) {
+        return null;
+    }
+
+    public List<Payment> getAllPayments() {
+        return null;
+    }
+}
